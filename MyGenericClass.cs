afa78dd317eaeb639decb9dae059c963f3cd9a06
forxynet <forxynet@gmail.com>
2020-06-12 22:11:47 +0300

    C# Generics Demo

---
+++ MyGenericClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsDemo {
+    public class MyGenericClass<T> {
+        //generic variable
+        //type fate type is generic
+
+        private T genericMemberVariable;
+        public MyGenericClass(T value) {
+            genericMemberVariable = value;
+        }
+
+        //generic method
+        //method accept one generic type paramater
+        //method return type also generic
+        public T genericMethod(T generekParamater) {
+            Console.WriteLine($"Parameter type {typeof(T)} value {generekParamater}");
+            Console.WriteLine($"Retun type {typeof(T)} value {genericMemberVariable}");
+            return genericMemberVariable;
+        }
+
+        //generic property
+        //the data type is generic
+        public T genericProperty { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo {
+    class Program {
+        static void Main(string[] args) {
+            //calculatewithgeneric();
+
+            MyGenericClass<float> integerGenericClass = new MyGenericClass<float>(10.1f);
+            var var = integerGenericClass.genericMethod(10.5f);
+            Console.ReadKey();
+        }
+
+        private static void calculatewithgeneric() {
+            int x = 10;
+            int y = 10;
+
+            bool IsEquel = ClsCalculator.AreEqual<double>(x, y);
+            if (IsEquel) {
+                Console.WriteLine("Both are Equel");
+            }
+            else {
+                Console.WriteLine("Both are not Equal");
+            }
+        }
+
+        public class ClsCalculator {
+            public static bool AreEqual<T>(T x, T y) {
+                return x.Equals(y);
+            }      
+        }
+    }
+}
